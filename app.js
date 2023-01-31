6fef09a27bce7109f89f2a0b9466cb75414920e2
Van Rys <tvanrys@fanshawec.ca>
2023-01-31 15:27:26 -0500

    messaging proof done

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const port = process.env.PORT || 3000;
+
+// tell express where to find static web files
+app.use(express.static('public'));
 
 // app.get is a route handler
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/contact.html');
-  });
 
 server.listen(port, () => {
   console.log(`listening on ${port}`);
+});
+
+// socket.io stuff goes here
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('connected', { sID: socket.id, message: 'new connection' });
+
+  // listen for incoming messages from ANYone connected to the chat service
+  // and then see what that message is
+  socket.on('chat_message', function(msg) { // step one - receive the message
+    console.log(msg);
+
+    // step 2 - show everyone what was just sent through (send the message to everyone connected to the service)
+    io.emit('new_message', { message: msg });
+  })
 });
